7dd3a1c8af29ae93ac58cc60f95af8652fa2c9a1
Mahsa Nurfarhan Hidayat <farhan@budiluhur.sch.id>
2025-10-26 13:13:25 +0700

    feat(ui): Add Google Fonts and Lucide Icons
    Integrates `google_fonts` and `lucide_icons_flutter` to enhance the application's user interface.
    
    - Adds `google_fonts` and `lucide_icons_flutter` as main dependencies.
    - Includes Open Sans font files as local assets in `assets/google_fonts/` to ensure availability and performance.
    - Adds all asset directories (animations, fonts, images, languages) to `pubspec.yaml`.
    - Implements a license registration method for the locally-hosted Open Sans font upon application startup.
    - Updates the minimum Flutter SDK constraint to `>=3.35.0`.

--- lib/budi_luhur/src/app/budi_luhur_initialize_app.dart
+++ lib/budi_luhur/src/app/budi_luhur_initialize_app.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:bl_e_school/budi_luhur/budi_luhur.dart';
+import 'package:bl_e_school/budi_luhur/src/app/init/google_fonts_licences.dart';
 import 'package:flutter/material.dart';
 
 /// Initializes all the necessary components before running the application.
@@ -8,6 +9,9 @@ Future<void> budiLuhurInitializeApp() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   HttpOverrides.global = MyHttpOverrides();
+
+  /// Registering the licences of [OpenSans](https://fonts.google.com/specimen/Open+Sans) font.
+  await registerGoogleFontsLicences();
 
   runApp(const BudiLuhurApp());
 }
---
+++ lib/budi_luhur/src/app/init/google_fonts_licences.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+/// A Method to register the licence of Font OpenSans.
+Future<void> registerGoogleFontsLicences() async {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('assets/google_fonts/OFL.txt');
+
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+}
